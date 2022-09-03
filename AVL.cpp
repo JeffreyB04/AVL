a936da2b98b7ca9e724a8a3cf46ebe82fa2c7128
JeffreyB04 <82740024+JeffreyB04@users.noreply.github.com>
2022-09-03 13:32:14 -0600

    Implemented

--- AVL.cpp
+++ AVL.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "AVL.h"
+
+int AVL::height(Nodo *) {
+    return 0;
+}
+
+int AVL::difference(Nodo *) {
+    return 0;
+}
+
+Nodo *AVL::rrRotation(Nodo *) {
+    return nullptr;
+}
+
+Nodo *AVL::llRotation(Nodo *) {
+    return nullptr;
+}
+
+Nodo *AVL::lrRotation(Nodo *) {
+    return nullptr;
+}
+
+Nodo *AVL::rlRotation(Nodo *) {
+    return nullptr;
+}
+
+Nodo *AVL::balance(Nodo *) {
+    return nullptr;
+}
+
+Nodo *AVL::insert(Nodo *, int) {
+    return nullptr;
+}
+
+void AVL::show(Nodo *, int) {
+
+}
+
+void AVL::inOrder(Nodo *) {
+
+}
+
+void AVL::preOrder(Nodo *) {
+
+}
+
+void AVL::postOrder(Nodo *) {
+
+}
+
+AVL::AVL() {
+
+}
--- AVL.h
+++ AVL.h
@@ -11,6 +11,16 @@ private:
     int height(Nodo*);
     int difference(Nodo*);
     Nodo *rrRotation(Nodo*);
+    Nodo *llRotation(Nodo*);
+    Nodo *lrRotation(Nodo*);
+    Nodo *rlRotation(Nodo*);
+    Nodo *balance(Nodo*);
+    Nodo *insert(Nodo*, int);
+    void show(Nodo*, int);
+    void inOrder(Nodo*);
+    void preOrder(Nodo*);
+    void postOrder(Nodo*);
+    AVL();
 };
 
 
